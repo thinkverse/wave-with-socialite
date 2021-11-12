d0d8f5a7307987867497e9eda235c19e75d0861f
thinkverse <hallberg.kim@gmail.com>
2021-11-13 00:14:02 +0100

    Fix provider_id column

--- database/migrations/2021_11_12_195257_add_socialite_fields_to_users_table.php
+++ database/migrations/2021_11_12_195257_add_socialite_fields_to_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('provider_id')->nullable();
+            $table->string('provider_id')->nullable()->unique();
         });
     }
 
